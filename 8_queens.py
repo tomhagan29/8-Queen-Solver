e568eb1d79b71e48070150960e20ad466d5492d9
tomhagan29 <tomhagan29@gmail.com>
2022-02-21 12:24:10 +0000

    Create 8_queens.py

---
+++ 8_queens.py
@@ -0,0 +1,64 @@
+import random
+
+class Node:
+    def __init__(self, board, parent=None):
+        self.board = board
+        self.parent = parent
+        if self.parent == None:
+            self.g = 0
+        else:
+            self.g = parent.g + 1
+        
+        @property
+        def f(self):
+            return self.g + self.h
+        
+        @property
+        def h(self):
+            return self.board.manhattan
+        
+            
+
+class Solver:
+    pass
+
+
+"""
+The N-Queens problem involves placing N queens on a NxN chess board so that no queen is able to take another.
+Therefore, a Queen cannot be in the same column, row or diagonal to another.
+Params: N (number of queens), k (last digit of student number), l (second last digit of student number)
+Note: in the board, 0 represents blank space and 1 represents a queen
+"""
+class Board:
+    def __init__(self, N, k, l):
+        self.board = [[0]*N for _ in range(N)]
+        self.k = k
+        self.l = l
+    
+    @property
+    def solved(self):
+        pass
+    
+    @property
+    def manhattan(self):
+        pass
+    
+    def shuffle(self):
+        for row in self.board:
+            i = random.randint(0,7)
+            row[i] = 1
+    
+    
+    
+    def pprint(self):
+        for row in self.board:
+            print(row)
+
+
+
+board = Board(8, 8, 7)
+board.shuffle()
+board.pprint()
+    
+        
+
