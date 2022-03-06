666254a2219337276ffbb363171dd8ab0d52333e
tomhagan29 <tomhagan29@gmail.com>
2022-03-06 12:19:53 +0000

    Final Comments

--- final.py
+++ final.py
@@ -2,7 +2,6 @@ from copy import deepcopy
 import numpy as np
 import random
 from collections import deque
-from pprint import pprint
 
 class Node:
     
@@ -30,16 +29,22 @@ class Node:
         return self.board.manhattan
     
     """
-    Check if solved
+    Returns true if solved
     """
     @property
     def solved(self):
         return self.board.solved
     
+    """
+    Returns the actions of the board
+    """
     @property
     def actions(self):
         return self.board.actions
     
+    """
+    Returns hashable representation
+    """
     @property
     def state(self):
         return str(self)
@@ -117,6 +122,9 @@ class Board:
         
         return queens
     
+    """
+    Returns all the possible actions on the board
+    """
     @property
     def actions(self):
         actions = dict()
@@ -126,6 +134,9 @@ class Board:
 
         return actions
     
+    """
+    Returns all the moves a specified queen can make
+    """
     def get_moves(self, queen):
         moves = []
         for i in range(self.N):
@@ -159,6 +170,9 @@ class Board:
         
         self.board[self.l][self.k] = 1
     
+    """
+    Returns the board as a string
+    """
     @property
     def hashable(self):
         return np.array2string(self.board)
@@ -176,7 +190,10 @@ class Solver:
     
     def __init__(self, start):
         self.start = start
-        
+    
+    """
+    A* search algorithm
+    """
     def solve(self):
         
         # Got number of solutions online at https://www.datagenetics.com/blog/august42012/
@@ -186,17 +203,23 @@ class Solver:
         queue = deque([Node(self.start)])
         seen = set()
         seen.add(queue[0].state)
+        # while the queue has elements
         while queue:
+            # sorting the queue by the node's objective function in ascending order
             queue = deque(sorted(list(queue), key=lambda node: node.f))
+            # taking the lowest heuristic node
             node = queue.popleft()
+            # checking if the node is solved
             if node.solved:
                 solutions.add(node.board.hashable)
             
+            # checking if all solutions are found
             if len(solutions) == number_of_solutions:
                 break
             
             steps += 1
             
+            # restarting the search
             if steps == 20:
                 new_board = Board(self.start.N, self.start.k, self.start.l)
                 new_board.populate_board()
@@ -206,15 +229,18 @@ class Solver:
                 node = queue.popleft()
                 steps = 0
 
-            
+            # for all the queens on the board
             for queen in node.board.queens:
+                # skipping the fixed queen
                 if queen == (node.board.k, node.board.l):
                     continue
+                # creating child nodes for each possible move
                 for action in node.actions[queen]:
                     new_board = deepcopy(node.board)
                     new_board.move(queen, action)
                     child = Node(new_board, node)
                     
+                    # adding the child to queue
                     if child.state not in seen:
                         queue.appendleft(child)
                         seen.add(child.state)
